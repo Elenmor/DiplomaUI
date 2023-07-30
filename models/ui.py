db06a7d8b734153ae12e0d46f55f31b984fc011b
Elena Morilova <elena.morilova@mail.ru>
2023-07-30 12:59:06 +0400

    add files

---
+++ models/ui.py
@@ -0,0 +1,69 @@
+from selene import have, command, be, by
+from selene.support.shared import browser
+
+class UIPage:
+
+    def open(self):
+        browser.open('https://www.dom-knigi.ru/')
+        return self
+
+    def login_form_open(self):
+        browser.element('//span[contains(@class, "icon-txt")]').should(have.text('Войти')).click()
+        return self
+
+    def login_name_type(self, name):
+        browser.element('[name="USER_LOGIN"]').type(name)
+        return self
+
+    def login_password_type(self, value):
+        browser.element('[name="USER_PASSWORD"]').type(value)
+        return self
+
+    def login_submit(self):
+        browser.element('[name="Login"]').press_enter()
+        return self
+
+    def login_error(self):
+        browser.element("//div[contains(text(), 'Неверный логин или пароль.')]").should(
+            be.visible)
+        return self
+
+    ##for search_test
+
+    def searh_input(self, value):
+        browser.element('#title-search-input').type(value).press_enter()
+        return self
+
+    def error_search(self):
+        browser.element("//font[contains(text(), 'К сожалению, на ваш поисковый запрос ничего не найдено.')]")
+
+    def search_book(self, value):
+        browser.element('#title-search-input').type(value).press_enter()
+        browser.element(by.partial_text(value)).should(
+            be.visible)
+
+    def add_to_cart(self):
+        browser.element('#bx_3966226736_711691_7e1b8e3524755c391129a9d7e6f2d206_buy_link').click()
+
+    def go_to_cart(self):
+        browser.element("//button[contains(text(), 'Перейти в корзину')]").click()
+
+    def cheack_cart(self):
+        browser.element(by.partial_text('Волкодав')).should(be.visible)
+
+    def press_delete(self):
+        browser.element('a[href="/basket/?clear"]').click()
+
+    def empty_cart(self):
+        browser.element(by.partial_text('Ваша корзина пуста')).should(be.visible)
+
+    def press_heart(self):
+        browser.element('#bx_3966226736_711691_7e1b8e3524755c391129a9d7e6f2d206').click()
+
+    def go_to_wishlist(self):
+        browser.element('#favour_in').click()
+
+
+
+    def cheack_wishlist(self):
+        browser.element(by.partial_text('Волкодав')).should(be.visible)
---
+++ test/test_ui.py
@@ -0,0 +1,129 @@
+from models.ui import UIPage
+import allure
+from allure_commons.types import Severity
+
+uipage = UIPage()
+
+
+@allure.tag("web")
+@allure.severity(Severity.CRITICAL)
+@allure.label("owner", "Morilova")
+@allure.description('Проверка авторизации')
+@allure.feature('Проверка невалидной авторизации')
+@allure.link('https://www.dom-knigi.ru/')
+def test_error_auth():
+    with allure.step('Открываем главную страницу'):
+        uipage.open()
+    with allure.step('Открываем форму авторизации'):
+        uipage.login_form_open()
+    with allure.step('Вводим неверный логин'):
+        uipage.login_name_type('gfigwwf')
+    with allure.step('Вводим неверный пароль'):
+        uipage.login_password_type('12345')
+    with allure.step('Нажимаем кнопку Войти'):
+        uipage.login_submit()
+    with allure.step('Проверяем наличие ошибки'):
+        uipage.login_error()
+@allure.tag("web")
+@allure.severity(Severity.CRITICAL)
+@allure.label("owner", "Morilova")
+@allure.description('Проверка поиска')
+@allure.feature('Проверка поиска отсутствующей книги')
+@allure.link('https://www.dom-knigi.ru/')
+def test_error_search():
+    with allure.step('Открываем главную страницу'):
+        uipage.open()
+    with allure.step('В строке поиска вводим набор букв'):
+        uipage.searh_input('rfhvgjk')
+    with allure.step('Проверяем наличие ошибки'):
+        uipage.error_search()
+
+@allure.tag("web")
+@allure.severity(Severity.CRITICAL)
+@allure.label("owner", "Morilova")
+@allure.description('Проверка поиска')
+@allure.feature('Проверка поиска существующей книги')
+@allure.link('https://www.dom-knigi.ru/')
+def test_search_book():
+    with allure.step('Открываем главную страницу'):
+        uipage.open()
+    with allure.step('Вводми название книги с строке поиска и проверяем ее наличие'):
+        uipage.search_book('Мастер и Маргарита')
+
+@allure.tag("web")
+@allure.severity(Severity.CRITICAL)
+@allure.label("owner", "Morilova")
+@allure.description('Проверка корзины')
+@allure.feature('Проверка добавления товара в корзину')
+@allure.link('https://www.dom-knigi.ru/')
+def test_add_to_cart():
+    with allure.step('Открываем главную страницу'):
+        uipage.open()
+    with allure.step('Ищем книгу'):
+        uipage.search_book('Волкодав')
+    with allure.step('Добавляем книгу в корзину'):
+        uipage.add_to_cart()
+    with allure.step('Переходим в корзину'):
+        uipage.go_to_cart()
+    with allure.step('Проверяем наличие книги в корзине'):
+        uipage.cheack_cart()
+
+@allure.tag("web")
+@allure.severity(Severity.CRITICAL)
+@allure.label("owner", "Morilova")
+@allure.description('Проверка корзины')
+@allure.feature('Проверка очистки корзины')
+@allure.link('https://www.dom-knigi.ru/')
+def test_clear_cart():
+    with allure.step('Открываем главную страницу'):
+        uipage.open()
+    with allure.step('Ищем книгу'):
+        uipage.search_book('Волкодав')
+    with allure.step('Добавляем книгу в корзину'):
+        uipage.add_to_cart()
+    with allure.step('Переходим в корзину'):
+        uipage.go_to_cart()
+    with allure.step('Нажимаем кнопку Очисить корзину'):
+        uipage.press_delete()
+    with allure.step('Проверяем, что корзина пуста'):
+        uipage.empty_cart()
+
+@allure.tag("web")
+@allure.severity(Severity.CRITICAL)
+@allure.label("owner", "Morilova")
+@allure.description('Проверка избранного')
+@allure.feature('Проверка добавления в избранное')
+@allure.link('https://www.dom-knigi.ru/')
+def test_add_wishlist():
+    with allure.step('Открываем главную страницу'):
+        uipage.open()
+    with allure.step('Ищем книгу'):
+        uipage.search_book('Волкодав')
+    with allure.step('Нажимаем сердечко'):
+        uipage.press_heart()
+    with allure.step('Переходим в избранное'):
+        uipage.go_to_wishlist()
+    with allure.step('Проверяем наличие книги в избранном'):
+        uipage.cheack_wishlist()
+
+@allure.tag("web")
+@allure.severity(Severity.CRITICAL)
+@allure.label("owner", "Morilova")
+@allure.description('Проверка избранного')
+@allure.feature('Проверка добавления в корзину из избранного')
+@allure.link('https://www.dom-knigi.ru/')
+def test_add_cart_from_wishlist():
+    with allure.step('Открываем главную страницу'):
+        uipage.open()
+    with allure.step('Ищем книгу'):
+        uipage.search_book('Волкодав')
+    with allure.step('Нажимаем сердечко'):
+        uipage.press_heart()
+    with allure.step('Переходим в избранное'):
+        uipage.go_to_wishlist()
+    with allure.step('Добавляем книгу в корзину'):
+        uipage.add_to_cart()
+    with allure.step('Переходим в корзину'):
+        uipage.go_to_cart()
+    with allure.step('Проверяем наличие книги в корзине'):
+        uipage.cheack_cart()
